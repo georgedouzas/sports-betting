e3da9dbfab3de9319dd5c61a20c57a589cc06ef4
georgedouzas <gdouzas@icloud.com>
2018-08-17 12:22:16 +0300

    Import the correct function

--- sportsbet/soccer/__init__.py
+++ sportsbet/soccer/__init__.py
@@ -1,6 +1,6 @@
-from .runner import fetch_raw_data, generate_simulation_results
+from .runner import fetch_simulation_data, generate_simulation_results
 
 __all__ = [
-    'fetch_raw_data',
+    'fetch_simulation_data',
     'generate_simulation_results'
 ]
