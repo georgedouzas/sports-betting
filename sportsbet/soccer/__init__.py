636d376475d962c96789e4650c3c6e1a18d6ece2
georgedouzas <gdouzas@icloud.com>
2018-08-18 18:41:14 +0300

    Simple import for function to generate grid scores

--- sportsbet/soccer/__init__.py
+++ sportsbet/soccer/__init__.py
@@ -1,6 +1,11 @@
-from .runner import fetch_simulation_data, generate_simulation_results
+from .runner import (
+    fetch_simulation_data, 
+    generate_simulation_results,
+    generate_grid_scores
+)
 
 __all__ = [
     'fetch_simulation_data',
-    'generate_simulation_results'
+    'generate_simulation_results',
+    'generate_grid_scores'
 ]
