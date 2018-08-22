90ca57438e569779adba141fd755acc9b3275a5a
georgedouzas <gdouzas@icloud.com>
2018-08-22 23:43:44 +0300

    Removed parameters and applied default values

--- sportsbet/soccer/runner.py
+++ sportsbet/soccer/runner.py
@@ -10,12 +10,13 @@ from warnings import filterwarnings
 import numpy as np
 import pandas as pd
 from .configuration import ( 
-    ESTIMATOR, 
-    PARAM_GRID,
+    ESTIMATORS, 
+    PARAM_GRIDS,
     FIT_PARAMS,
     MIN_N_MATCHES,
-    ODDS_THRESHOLD, 
-    GENERATE_WEIGHTS
+    ODDS_THRESHOLDS, 
+    GENERATE_WEIGHTS,
+    SCORERS_MAPPING
 )
 from .data import fetch_raw_data, extract_training_data, extract_odds_dataset
 from .optimization import Betting
@@ -32,23 +33,21 @@ def fetch_simulation_data(dirpath=None):
     return training_data, odds_data
 
 
-def generate_grid_scores(estimator=ESTIMATOR, param_grid=PARAM_GRID, fit_params=FIT_PARAMS, dirpath='data',
-                                test_season='17-18', predicted_result='D', min_matches=MIN_N_MATCHES, random_state=None):
+def generate_grid_scores(predicted_result, dirpath='data', test_season='17-18', random_state=None):
     """Generate the grid scores of a test season."""
     filterwarnings('ignore')
     training_data = pd.read_csv(join(dirpath, 'training_data.csv'))
-    betting = Betting(estimator, param_grid, fit_params)
-    grid_scores = betting.return_grid_scores(training_data, test_season, predicted_result, min_matches, random_state)
+    betting = Betting(ESTIMATORS[predicted_result], PARAM_GRIDS[predicted_result], FIT_PARAMS[predicted_result])
+    grid_scores = betting.return_grid_scores(SCORERS_MAPPING[predicted_result], training_data, test_season, MIN_N_MATCHES, random_state)
     return grid_scores
 
 
-def generate_simulation_results(estimator=ESTIMATOR, param_grid=PARAM_GRID, fit_params=FIT_PARAMS, dirpath='data',
-                                test_season='17-18', predicted_result='D', min_matches=MIN_N_MATCHES, 
-                                odds_threshold=ODDS_THRESHOLD, generate_weights=GENERATE_WEIGHTS, random_state=None):
+def generate_simulation_results(predicted_result, dirpath='data', test_season='17-18', generate_weights=GENERATE_WEIGHTS, random_state=None):
     """Run betting simulation and generate the results."""
     filterwarnings('ignore')
     training_data = pd.read_csv(join(dirpath, 'training_data.csv'))
     odds_data = pd.read_csv(join(dirpath, 'odds_data.csv'))
-    betting = Betting(estimator, param_grid, fit_params)
-    results = betting.simulate_results(training_data, odds_data, test_season, predicted_result, min_matches, odds_threshold, generate_weights, random_state)
+    betting = Betting(ESTIMATORS[predicted_result], PARAM_GRIDS[predicted_result], FIT_PARAMS[predicted_result])
+    results = betting.simulate_results(training_data, odds_data, test_season, predicted_result, MIN_N_MATCHES, 
+                                       ODDS_THRESHOLDS[predicted_result], generate_weights, random_state)
     return results
