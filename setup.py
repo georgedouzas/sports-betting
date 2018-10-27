d23bb2d874f99829b7e8e7ed38c2da000c2cfae5
georgedouzas <georgios.douzas@gmail.com>
2018-10-27 11:28:15 +0300

    Adde sklearn and dependencies

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
+        'progressbar2',
         'scipy',
         'numpy',
         'pandas',
-        'scikit-learn'
+        'scikit-learn==0.19.2',
+        'imbalanced-learn==0.3.3',
+        'category_encoders',
+
     ]
 )
