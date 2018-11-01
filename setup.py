d736158b2a2d7ecaceacb53f4cd86d4eda539020
georgedouzas <georgios.douzas@gmail.com>
2018-11-01 04:54:15 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'progressbar2',
-        'scipy',
-        'numpy',
-        'pandas',
-        'scikit-learn==0.19.2',
-        'imbalanced-learn==0.3.3',
-        'category_encoders',
-
+        'scipy==1.1.0',
+        'numpy==1.15.3',
+        'pandas==0.23.4',
+        'scikit-learn==0.20',
+        'imbalanced-learn==0.4.2',
+        'progressbar2==3.38.0'
+    ],
+    scripts=[
+        'sportsbet/scripts/fetch_training_data',
+        'sportsbet/scripts/backtesting',
     ]
 )
