047171d0a269e710131a06d60ebd63019efab96f
georgedouzas <gdouzas@icloud.com>
2018-11-25 04:36:58 +0200

    Keep only the path

--- sportsbet/__init__.py
+++ sportsbet/__init__.py
@@ -8,17 +8,11 @@ from sys import path
 
 from sklearn.metrics import SCORERS, make_scorer
 
-from .utils import yield_score
-
 # Define default path
 PATH = join(str(Path.home()), '.sports-betting')
-
-# Append default path
 path.append(PATH)
 
-# Append scorers
-named_scorers = {'mean_profit': yield_score}
-for name, scorer in named_scorers.items():
-    SCORERS[name] = make_scorer(scorer)
+
+
 
 
