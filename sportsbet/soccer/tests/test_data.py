ff877775c40d34f5e8a5df7cb7fe171f5a472454
georgedouzas <georgios.douzas@gmail.com>
2018-12-27 14:41:11 +0200

    Added transform test for fixtures

--- sportsbet/soccer/tests/test_data.py
+++ sportsbet/soccer/tests/test_data.py
@@ -5,7 +5,13 @@ Test the data module.
 import requests
 import numpy as np
 
-from sportsbet.soccer.data import SPIDataSource, FDDataSource, FDTrainingDataSource, FDFixturesDataSource
+from sportsbet.soccer.data import (
+    SPIDataSource, 
+    FDDataSource, 
+    FDTrainingDataSource, 
+    FDFixturesDataSource,
+    SoccerDataLoader
+)
 
 SPI_DATA_SOURCE = SPIDataSource(['E0', 'G1']).download()
 FD_TRAINING_DATA_SOURCE = FDTrainingDataSource('SP1', '1819').download()
@@ -67,6 +73,7 @@ def test_fd_training_download():
     """Test FD training download."""
     assert hasattr(FD_TRAINING_DATA_SOURCE, 'content_')
     assert set(FD_TRAINING_DATA_SOURCE.content_.columns) == set(FD_TRAINING_DATA_SOURCE.match_cols + FD_TRAINING_DATA_SOURCE.full_goals_cols + FD_TRAINING_DATA_SOURCE.half_goals_cols + FD_TRAINING_DATA_SOURCE.avg_max_odds_cols + FD_TRAINING_DATA_SOURCE.odds_cols)
+    assert set(FD_TRAINING_DATA_SOURCE.content_.Div.unique()) == set([FD_TRAINING_DATA_SOURCE.league_id])
     
 
 def test_fd_training_transform():
@@ -77,6 +84,13 @@ def test_fd_training_transform():
 
 
 def test_fd_fixtures_download():
-    """Test FD training download."""
+    """Test FD fixtures download."""
     assert hasattr(FD_FIXTURES_DATA_SOURCE, 'content_')
     assert set(FD_FIXTURES_DATA_SOURCE.content_.columns) == set(FD_FIXTURES_DATA_SOURCE.match_cols + FD_FIXTURES_DATA_SOURCE.avg_max_odds_cols + FD_FIXTURES_DATA_SOURCE.odds_cols)
+
+
+def test_fd_fixtures_transform():
+    """Test Fd training transform."""
+    content = FD_FIXTURES_DATA_SOURCE.transform()
+    assert np.issubclass_(content.Date.dtype.type, np.datetime64)
+    assert set(content.Div.unique()).issubset(FD_FIXTURES_DATA_SOURCE.leagues_ids)
