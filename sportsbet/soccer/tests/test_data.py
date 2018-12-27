c66165f05d636b3f43e38dde17ad73b24750b633
georgedouzas <gdouzas@icloud.com>
2018-12-27 14:00:57 +0200

    Finished data sources tests

--- sportsbet/soccer/tests/test_data.py
+++ sportsbet/soccer/tests/test_data.py
@@ -3,10 +3,13 @@ Test the data module.
 """
 
 import requests
+import numpy as np
 
-from sportsbet.soccer.data import SPIDataSource, FDDataSource
+from sportsbet.soccer.data import SPIDataSource, FDDataSource, FDTrainingDataSource, FDFixturesDataSource
 
 SPI_DATA_SOURCE = SPIDataSource(['E0', 'G1']).download()
+FD_TRAINING_DATA_SOURCE = FDTrainingDataSource('SP1', '1819').download()
+FD_FIXTURES_DATA_SOURCE = FDFixturesDataSource(['SP1', 'I1']).download()
 
 
 def test_spi_connection():
@@ -32,8 +35,15 @@ def test_spi_download():
 
 def test_spi_transform():
     """Test SPI transform."""
-    content = SPI_DATA_SOURCE.content_
-    print(type(content.date))
+    content = SPI_DATA_SOURCE.transform()
+    assert np.issubclass_(content[0].date.dtype.type, np.datetime64)
+    assert np.issubclass_(content[1].date.dtype.type, np.datetime64)
+    assert set(content[0].league.unique()) == set(SPI_DATA_SOURCE.leagues_ids)
+    assert set(content[1].league.unique()) == set(SPI_DATA_SOURCE.leagues_ids)
+    assert content[0].score1.isna().sum() == 0
+    assert content[0].score2.isna().sum() == 0
+    assert content[1].score1.isna().sum() == content[1].score1.size
+    assert content[1].score2.isna().sum() == content[1].score2.size
 
 
 def test_fd_connection():
@@ -41,4 +51,32 @@ def test_fd_connection():
     status_code = requests.head(FDDataSource.base_url).status_code
     assert status_code == 200
 
+
+def test_fd_initialization():
+    """Test FD initialization."""
+    fd_data_source = FDDataSource()
+    assert fd_data_source.base_url == 'http://www.football-data.co.uk'
+    assert fd_data_source.match_cols == ['Date', 'Div', 'HomeTeam', 'AwayTeam']
+    assert fd_data_source.full_goals_cols == ['FTHG', 'FTAG']
+    assert fd_data_source.half_goals_cols == ['HTHG', 'HTAG']
+    assert fd_data_source.avg_max_odds_cols == ['BbAvH', 'BbAvD', 'BbAvA', 'BbMxH', 'BbMxD', 'BbMxA']
+    assert fd_data_source.odds_cols == ['PSH', 'PSD', 'PSA', 'B365H', 'B365D', 'B365A', 'BWH', 'BWD', 'BWA']
+
+
+def test_fd_training_download():
+    """Test FD training download."""
+    assert hasattr(FD_TRAINING_DATA_SOURCE, 'content_')
+    assert set(FD_TRAINING_DATA_SOURCE.content_.columns) == set(FD_TRAINING_DATA_SOURCE.match_cols + FD_TRAINING_DATA_SOURCE.full_goals_cols + FD_TRAINING_DATA_SOURCE.half_goals_cols + FD_TRAINING_DATA_SOURCE.avg_max_odds_cols + FD_TRAINING_DATA_SOURCE.odds_cols)
     
+
+def test_fd_training_transform():
+    """Test Fd training transform."""
+    content = FD_TRAINING_DATA_SOURCE.transform()
+    assert np.issubclass_(content.Date.dtype.type, np.datetime64)
+    assert set(content.Season.unique()) == set([FD_TRAINING_DATA_SOURCE.season])
+
+
+def test_fd_fixtures_download():
+    """Test FD training download."""
+    assert hasattr(FD_FIXTURES_DATA_SOURCE, 'content_')
+    assert set(FD_FIXTURES_DATA_SOURCE.content_.columns) == set(FD_FIXTURES_DATA_SOURCE.match_cols + FD_FIXTURES_DATA_SOURCE.avg_max_odds_cols + FD_FIXTURES_DATA_SOURCE.odds_cols)
