09060619b150c8e9cbce9bdc6dc676bdc7f60b4d
georgedouzas <georgios.douzas@gmail.com>
2019-02-15 01:33:39 +0200

    Create data directory and childs

--- sportsbet/__init__.py
+++ sportsbet/__init__.py
@@ -3,8 +3,10 @@ Defines the data path.
 """
 
 from os.path import join, dirname
+from pathlib import Path
 
 PATH = join(dirname(__file__), '..', 'data')
+Path(PATH).mkdir(exist_ok=True)
 
 
 
--- sportsbet/soccer/__init__.py
+++ sportsbet/soccer/__init__.py
@@ -3,9 +3,11 @@ Defines the soccer data path.
 """
 
 from os.path import join
+from pathlib import Path
 
 from .. import PATH
 
 SOCCER_PATH = join(PATH, 'soccer')
+Path(SOCCER_PATH).mkdir(exist_ok=True)
 
 
