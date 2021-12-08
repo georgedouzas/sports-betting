4a0989069470a4777a43512ca88930b4d60d8a35
georgedouzas <gdouzas@icloud.com>
2021-12-08 17:25:00 +0200

    MNT fixed conda build

--- conda-recipe/conda_deployment.sh
+++ conda-recipe/conda_deployment.sh
@@ -2,5 +2,5 @@
 
 mkdir build_conda
 conda config --set anaconda_upload yes
-conda build --output-folder ./build_conda --user AlgoWit sports-betting
+conda build --output-folder ./build_conda --user gdouzas sports-betting
 rm -r build_conda
