20ae8cb4cf8db4fd83dc2126ce8cff56d0043e81
georgedouzas <gdouzas@icloud.com>
2021-12-15 17:21:35 +0200

    Added FD and FTE examples to extract data

---
+++ examples/extract_fd_soccer_data.py
@@ -0,0 +1,97 @@
+"""
+==================================
+Extracting data from Football-Data
+==================================
+
+This example illustrates the usage of Football-Data dataloader.
+
+"""
+
+# Author: Georgios Douzas <gdouzas@icloud.com>
+# Licence: MIT
+
+import pandas as pd
+from sportsbet.datasets import FDSoccerDataLoader
+
+###############################################################################
+# Getting the available parameters
+###############################################################################
+
+###############################################################################
+# We can get the available parameters in order to select the training data
+# to be extracted, using the :meth:`get_all_params` class method.
+
+params = FDSoccerDataLoader.get_all_params()
+
+###############################################################################
+# The available parameters can be presented as a DataFrame.
+
+params_df = pd.DataFrame(params).sort_values(
+    ['league', 'year', 'division'], ignore_index=True
+)
+params_df
+
+###############################################################################
+# We select to extract training data only for the year 2021 of the first
+# division Spanish and Italian leagues.
+
+param_grid = {'league': ['Spain', 'Italy'], 'division': [1], 'year': [2021]}
+
+###############################################################################
+# Getting the available odds types
+###############################################################################
+
+###############################################################################
+# We can get the available odds types in order to match the output of the
+# training data, using the :meth:`get_odds_types` class method.
+
+FDSoccerDataLoader.get_odds_types()
+
+###############################################################################
+# We select the odds types to be the market average.
+
+odds_type = 'market_average'
+
+###############################################################################
+# Extracting the training data
+###############################################################################
+
+###############################################################################
+# We extract the training data, keeping columns and rows with non missing
+# values by setting the `drop_na_thres` parameter equal to `1.0`.
+
+dataloader = FDSoccerDataLoader(param_grid=param_grid)
+X_train, Y_train, Odds_train = dataloader.extract_train_data(
+    drop_na_thres=1.0, odds_type=odds_type
+)
+
+###############################################################################
+# The input data:
+X_train
+
+###############################################################################
+# The targets:
+Y_train
+
+###############################################################################
+# The market average odds:
+Odds_train
+
+###############################################################################
+# Extracting the fixtures data
+###############################################################################
+
+###############################################################################
+# We extract the fixtures data with columns that match the columns of the
+# training data. On the other hand, the fixtures data are not affected by
+# the `param_grid` selection.
+
+X_fix, _, Odds_fix = dataloader.extract_fixtures_data()
+
+###############################################################################
+# The input data:
+X_fix
+
+###############################################################################
+# The market average odds:
+Odds_fix
---
+++ examples/extract_fte_soccer_data.py
@@ -0,0 +1,85 @@
+"""
+======================================
+Extracting data from FiveThirtyEight
+======================================
+
+This example illustrates the usage of FiveThirtyEight dataloader.
+
+"""
+
+# Author: Georgios Douzas <gdouzas@icloud.com>
+# Licence: MIT
+
+import pandas as pd
+from sportsbet.datasets import FTESoccerDataLoader
+
+###############################################################################
+# Getting the available parameters
+###############################################################################
+
+###############################################################################
+# We can get the available parameters in order to select the training data
+# to be extracted, using the :meth:`get_all_params` class method.
+
+params = FTESoccerDataLoader.get_all_params()
+
+###############################################################################
+# The available parameters can be presented as a DataFrame.
+
+params_df = pd.DataFrame(params).sort_values(
+    ['league', 'year', 'division'], ignore_index=True
+)
+params_df
+
+###############################################################################
+# We select to extract training data only for the year 2021 of all the
+# divisions of English league.
+
+param_grid = {'league': ['England'], 'year': [2021]}
+
+###############################################################################
+# Getting the available odds types
+###############################################################################
+
+###############################################################################
+# We can get the available odds types in order to match the output of the
+# training data, using the :meth:`get_odds_types` class method.
+
+FTESoccerDataLoader.get_odds_types()
+
+###############################################################################
+# Therefore no odds data are available.
+
+###############################################################################
+# Extracting the training data
+###############################################################################
+
+###############################################################################
+# We extract the training data using the default values for the parameters
+# `odds_type` and `drop_na_thres`.
+
+dataloader = FTESoccerDataLoader(param_grid=param_grid)
+X_train, Y_train, _ = dataloader.extract_train_data()
+
+###############################################################################
+# The input data:
+X_train
+
+###############################################################################
+# The targets:
+Y_train
+
+###############################################################################
+# Extracting the fixtures data
+###############################################################################
+
+###############################################################################
+# We extract the fixtures data with columns that match the columns of the
+# training data. On the other hand, the fixtures data are not affected by
+# the `param_grid` selection.
+
+X_fix, *_ = dataloader.extract_fixtures_data()
+
+###############################################################################
+# The input data:
+X_fix
