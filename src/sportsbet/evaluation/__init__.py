c2326249bd0f616c845cd17ffe9b04237c28ce7b
georgedouzas <gdouzas@icloud.com>
2023-04-24 11:55:33 +0300

    refactor: Modify base class and subclasses
    - Add progress bar to backtesting iterations
    - Remove plot function

--- src/sportsbet/evaluation/__init__.py
+++ src/sportsbet/evaluation/__init__.py
@@ -2,7 +2,12 @@
 
 from __future__ import annotations
 
+from ._base import load_bettor
 from ._classifier import ClassifierBettor
 from ._rules import OddsComparisonBettor
 
-__all__: list[str] = ['ClassifierBettor', 'OddsComparisonBettor']
+__all__: list[str] = [
+    'ClassifierBettor',
+    'OddsComparisonBettor',
+    'load_bettor',
+]
