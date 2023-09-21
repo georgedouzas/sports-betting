1f411c42d9555402e345bbbe15905815d5f56720
georgedouzas <gdouzas@icloud.com>
2023-09-21 18:09:12 +0300

    style: Apply ruff guide

--- configs/dataloaders/main_leagues.py
+++ configs/dataloaders/main_leagues.py
@@ -4,13 +4,13 @@ from sportsbet.datasets import SoccerDataLoader
 
 MAIN = {
     'dataloader': SoccerDataLoader,
-    'path': './data/main_leagues.pkl'
+    'path': './data/main_leagues.pkl',
 }
 OPTIONAL = {
     'param_grid': {
        'league': ['England', 'Scotland', 'Germany', 'Italy', 'Spain', 'France', 'Netherlands', 'Belgium', 'Portugal', 'Turkey', 'Greece'],
-        'year': [2017, 2018, 2019, 2020, 2021, 2022, 2023]
+        'year': [2017, 2018, 2019, 2020, 2021, 2022, 2023],
     },
     'drop_na_thres': 0.8,
-    'odds_type': 'market_maximum'
+    'odds_type': 'market_maximum',
 }
